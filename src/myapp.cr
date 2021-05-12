6a4a461bc320eb345628cfe0f48cbc3742ac708d
SimaDovakin <simadovakin999@gmail.com>
2021-05-12 12:15:28 +0300

    Add some views and routes

--- src/myapp.cr
+++ src/myapp.cr
@@ -4,4 +4,18 @@ get "/" do
   "<h1>Hello World!!!</h1>"
 end
 
+get "/another-section" do
+  "<h1>Just another section</h1>"
+end
+
+get "/users/:name" do |env|
+  name = env.params.url["name"]
+  render "src/views/hello.ecr"
+end
+
+get "/list" do
+  list = ["first", "second", "third", "fourth", "fifth"]
+  render "src/views/list.ecr"
+end
+
 Kemal.run
